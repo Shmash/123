f73979247d3d8a51a652335443139fa92c7ad2a6
Shmash <35150690+Shmash@users.noreply.github.com>
2018-01-09 20:04:27 +0100

    updated update.sh more simple now

--- update.sh
+++ update.sh
@@ -1,12 +1,24 @@
-cd /home/ethos/configGit/123; pwd
-
-if git pull | grep -q 'Already up-to-date.'; then
-        echo "up to date"
-else
-        cd ..
-        rm ccminer
-        cp 123/ccminer .
-        echo "-eworker $1" >> ccminer
-        echo "-dwal $2" >> ccminer
-        echo "Updated"
-fi
+cd /home/ethos/configGit/123; pwd                                                                                                     
+                                                                                                                                      
+if [ $1 = "force" ]; then                                                                                                             
+        # Forcing creaton of ccminer with correct worker parameters                                                                   
+        # this is usually called the first time.                                                                                      
+        cd ..                                                                                                                         
+        rm ccminer                                                                                                                    
+        cp 123/ccminer .                                                                                                              
+        echo "-eworker $2" >> ccminer                                                                                                 
+        echo "-dwal $3" >> ccminer                                                                                                    
+        cd ..                                                                                                                         
+        rm claymore.stub.conf                                                                                                         
+        ln -s configGit/ccminer claymore.stub.conf                                                                                    
+        echo "force updated ccminer"                                                                                                  
+elif git pull | grep -q 'Already up-to-date.'; then                                                                                   
+        echo "up to date"                                                                                                             
+else                                                                                                                                  
+        cd ..                                                                                                                         
+        rm ccminer                                                                                                                    
+        cp 123/ccminer .                                                                                                              
+        echo "-eworker $1" >> ccminer                                                                                                 
+        echo "-dwal $2" >> ccminer                                                                                                    
+        echo "Updated"                                                                                                                
+fi                                                                                                                                    
